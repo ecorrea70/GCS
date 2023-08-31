5b986b186371f890171a7e7410fbf89172b8e057
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-08-31 00:37:36 -0300

    criação dos métodos A e D

--- App.java
+++ App.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Dudu!");
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(5);
+        lista.add(5);
+        lista.add(3);
+        lista.add(3);
+
+        System.out.println("Número de ocorrências do número 5: "+nOcorrencias(lista, 5));
+        System.out.println("Valores repetidos: "+listRepeat(lista));
+    }
+    public static int nOcorrencias(ArrayList<Integer> l, Integer el) {
+        int count = 0;
+        for (Integer num : l) {
+            if (num.equals(el)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> repetidos = new ArrayList<>();
+        ArrayList<Integer> lidos = new ArrayList<>();
+
+        for (int i = 0; i < l.size(); i++) {
+            int valor = l.get(i);
+            if (lidos.contains(valor)) {
+                continue;
+            }
+
+            boolean ehRepetido = false;
+            for (int j = i + 1; j < l.size(); j++) {
+                if (valor == l.get(j)) {
+                    ehRepetido = true;
+                    repetidos.add(valor);
+                    break;
+                }
+            }
+
+            if (ehRepetido) {
+                lidos.add(valor);
+            }
+        }
+
+        return repetidos;
     }
 }
